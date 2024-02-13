5169eceeae5632cadacca4b8896b7e4d30e574ea
Ad3Ni1 <danielvincentdelacruz99@gmail.com>
2024-02-13 14:12:06 +0800

    huge commit

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <?php
-
+session_start();
+include "db_conn.php";
 if (isset($_POST['uname']) && isset($_POST['pword'])){
     function validate($data){
         $data = trim($data);
@@ -18,7 +19,28 @@ if (isset($_POST['uname']) && isset($_POST['pword'])){
         header("Location: LoginPage.php?error=Password is required");
         exit();
     }else{
-        echo "Logged in";
+        $sql = "SELECT * FROM users WHERE user_name = '$uname' AND password='$pass'";
+
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result)){
+            $row = mysqli_fetch_assoc($result);
+            if($row['user_name'] === $uname && $row['password'] === $pass){
+                $_SESSION['user_name'] = $row['user_name'];
+                $_SESSION['name'] = $row['name'];
+                $_SESSION['id'] = $row['id'];
+                header("Location: homepage.php");
+                exit();
+
+            }else{
+                header("Location: LoginPage.php?error=Incorrect username or password");
+                exit();
+            }
+
+        }else{
+            header("Location: LoginPage.php?error=Incorrect username or password");
+            exit();
+        }
     }
 }else{
     header("Location: LoginPage.php");
