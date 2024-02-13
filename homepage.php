99b9840e8ffce180b7fbfd1e7bee8bc885f12823
Ad3Ni1 <danielvincentdelacruz99@gmail.com>
2024-02-13 14:25:42 +0800

    correction commit

--- homepage.php
+++ homepage.php
@@ -7,13 +7,11 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homepage</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    <h1>Hello, <?php echo $_SESSION['name'] ?></h1>
+    <h1>Hello, <?php echo $_SESSION['name']; ?></h1>
     <a href="logout.php">Logout</a>
 </body>
 </html>
